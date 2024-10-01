4bf7f2eb9af1b2ee8e811c42390013c45572f7e5
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2024-10-01 21:07:02 +0530

    minor changes

--- src/routes/Checkout.jsx
+++ src/routes/Checkout.jsx
@@ -4,7 +4,7 @@ import { replace, useNavigate } from "react-router-dom";
 export default function Checkout() {
   const navigate = useNavigate();
   return (
-    <div className="flex flex-col justify-center items-center h-[60dvh] w-full text-center">
+    <div className="flex flex-col justify-center items-center h-dvh w-full text-center">
       <h1 className="font-bold text-6xl pb-5">
         Thank you for <br /> your order
       </h1>
